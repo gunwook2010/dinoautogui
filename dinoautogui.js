73207db7e2486c3e5b1874769fd1dc9141fb1752
gunwook2010 <57968248+gunwook2010@users.noreply.github.com>
2022-10-23 20:54:46 +0900

    삽입 까지 완성
    기능 추가하고 압축 할 수 있으면 할 예정

--- dinoautogui.js
+++ dinoautogui.js
@@ -1,5 +1,3 @@
-// wip 제작중...
-
 
 function dinoautogui_cdiv(){
   let game = document.getElementById('t'); // 게임(t) 가져오기 선언
@@ -16,13 +14,60 @@ function dinoautogui_cdiv(){
 
 // css 삽입
 
-  let code = document.createElement('style'); // style 코드 선언
+  let css = document.createElement('style'); // style 코드 선언
   //var gamecode = game // 게임(t)코드 복사
 
-  code.setAttribute('id', 'dinoguicss'); // id 선언
-  code.innerHTML =  // css 코드 설정
+  css.setAttribute('id', 'dinoguicss'); // id 선언
+  css.innerHTML = "#dinogui{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#dinoguiheader{padding:10px;cursor:move;z-index:10;background-color:#2196F3;color:#fff}" // css 코드 설정
   
-  game.appendChild(code); // 게임(t) 코드 삽입
+  game.appendChild(css); // 게임(t) 코드 삽입
 
 
+}
+
+dinoautogui_cdiv() // html,css 삽입 코드 호출
+
+// js 코드
+
+dragElement(document.getElementById("dinogui"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
