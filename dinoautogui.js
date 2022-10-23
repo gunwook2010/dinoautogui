201e2e335df1169156366c4e8ebc3e410cf84a4b
gunwook2010 <57968248+gunwook2010@users.noreply.github.com>
2022-10-24 07:58:33 +0900

    Update dinoautogui.js
    무적 기능까지

--- dinoautogui.js
+++ dinoautogui.js
@@ -1,72 +1,108 @@
-
 function dinoautogui_cdiv(){
-  let game = document.getElementById('t'); // 게임(t) 가져오기 선언
+  let game = document.getElementById('t'); 
 
-// html 삽입
+  let code = document.createElement('div'); 
+  gamecodet = document.createElement('div'); 
 
-  let code = document.createElement('div'); // div 코드 선언
-  //var gamecode = game // 게임(t)코드 복사
-
-  code.setAttribute('id', 'dinogui'); // id 선언
-  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><p>무적</p><p>속도</p><p>종료</p>"; // html 코드 설정
+  code.setAttribute('id', 'dinogui'); 
+  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><div id='dag_main'><button onclick='dag_im_on();'>무적</button> <br> <button onclick='dag_sp_on()'>속도</button> <br> <button onclick='dag_ex_on()'>종료(wip)</button></div> <div id='dag_im'> <button onclick='Runner.prototype.gameOver = function (){};'>무적모드</button> <br> <button onclick='Runner.prototype.gameOver = dino_over;'>무적모드 끄기</button> <br> <button onclick='back()'>돌아가기</button></div> <div id='dag_sp'>스피드 올리기내리기<button onclick='back()'>돌아가기</button></div><div id='dag_ex'>개발중<br><button onclick='back()'>돌아가기</button></div>"; 
   
-  game.appendChild(code); // 게임(t) 코드 삽입
+  game.appendChild(code); 
 
-// css 삽입
+  let css = document.createElement('style');
 
-  let css = document.createElement('style'); // style 코드 선언
-  //var gamecode = game // 게임(t)코드 복사
 
-  css.setAttribute('id', 'dinoguicss'); // id 선언
-  css.innerHTML = "#dinogui{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#dinoguiheader{padding:10px;cursor:move;z-index:10;background-color:#2196F3;color:#fff}" // css 코드 설정
+  css.setAttribute('id', 'dinoguicss');
+  css.innerHTML = "#dinogui{position:absolute;z-index:9;background-color:#f1f1f1;border:1px solid #d3d3d3;text-align:center}#dinoguiheader{padding:10px;cursor:move;z-index:10;background-color:#2196F3;color:#fff}"
   
-  game.appendChild(css); // 게임(t) 코드 삽입
+  game.appendChild(css);
 
 
 }
 
-dinoautogui_cdiv() // html,css 삽입 코드 호출
+var dino_over = Runner.prototype.gameOver
 
-// js 코드
+dinoautogui_cdiv()
+
+function dag_im_on(){
+  let dagim = document.getElementById('dag_im');
+  
+  let dagmain = document.getElementById('dag_main');
+  
+  dagim.style.display = "block";
+  dagmain.style.display = "none";
+}
+
+function dag_sp_on(){
+  let dagsp = document.getElementById('dag_sp');
+  
+  let dagmain = document.getElementById('dag_main');
+  
+  dagsp.style.display = "block";
+  dagmain.style.display = "none";
+}
+
+function dag_im_ex(){
+  let dagex = document.getElementById('dag_ex');
+  
+  let dagmain = document.getElementById('dag_main');
+  
+  dagex.style.display = "block";
+  dagmain.style.display = "none";
+}
+
+function back(){
+  dagim1 = document.getElementById('dag_im');
+  dagmain1 = document.getElementById('dag_main');
+  dagsp1 = document.getElementById('dag_sp');
+  dagex1 = document.getElementById('dag_ex');
+  dagim1.style.display = "none";
+  dagmain1.style.display = "block";
+  dagsp1.style.display = "none";
+  dagex1.style.display = "none";
+}
+
+dagim1 = document.getElementById('dag_im');
+dagmain1 = document.getElementById('dag_main');
+dagsp1 = document.getElementById('dag_sp');
+dagex1 = document.getElementById('dag_ex');
+dagim1.style.display = "none";
+dagmain1.style.display = "block";
+dagsp1.style.display = "none";
+dagex1.style.display = "none";
+
 
 dragElement(document.getElementById("dinogui"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
