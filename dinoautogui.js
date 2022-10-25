48edff0e646326b8f6304afdac52b1793759433b
gunwook2010 <57968248+gunwook2010@users.noreply.github.com>
2022-10-25 22:57:52 +0900

    Update dinoautogui.js

--- dinoautogui.js
+++ dinoautogui.js
@@ -1,3 +1,17 @@
+console.log('%c dinoautogui','font:5em sans-serif;color:blue;');
+dinolog('info', '로딩 시작');
+
+function dinolog(logt, logs){
+  let today = new Date();
+
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+  let ms = today.getMilliseconds();
+  console.log('[dinoautogui] [' + hour + ':' + min + ':' + sec + '.' + ms +'] [' + logt + '] ' + logs);
+}
+
+
 function dinoautogui_cdiv(){
   let game = document.getElementById('t'); 
 
@@ -5,7 +19,7 @@ function dinoautogui_cdiv(){
   gamecodet = document.createElement('div'); 
 
   code.setAttribute('id', 'dinogui'); 
-  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><div id='dag_main'><button onclick='dag_im_on();'>무적</button> <br> <button onclick='dag_sp_on()'>속도</button> <br> <button onclick='dag_ex_on()'>종료(wip)</button></div> <div id='dag_im'> <button onclick='Runner.prototype.gameOver = function (){};'>무적모드</button> <br> <button onclick='Runner.prototype.gameOver = dino_over;'>무적모드 끄기</button> <br> <button onclick='back()'>돌아가기</button></div> <div id='dag_sp'>스피드 올리기내리기<button onclick='back()'>돌아가기</button></div><div id='dag_ex'>개발중<br><button onclick='back()'>돌아가기</button></div>"; 
+  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><div id='dag_main'><button onclick='dag_im_on();'>무적</button> <br> <button onclick='dag_sp_on()'>속도</button> <br> <button onclick='dag_ex_on()'>종료(wip)</button></div> <div id='dag_im'> <button onclick='Runner.prototype.gameOver = function (){};'>무적모드</button> <br> <button onclick='Runner.prototype.gameOver = dino_over;'>무적모드 끄기</button> <br> <button onclick='back()'>돌아가기</button></div> <div id='dag_sp'><input id='dag_speed' type='number' style='ime-mode:disabled;'></input><button onclick='dag_setspeed()'>적용</button><br><button onclick='back()'>돌아가기</button></div><div id='dag_ex'>개발중<br><button onclick='dag_exit()' style='background-color: red;'>종료</button><br><button onclick='dagr()' style='background-color: red;'>리셋</button><br><button onclick='back()'>돌아가기</button></div>"; 
   
   game.appendChild(code); 
 
@@ -21,8 +35,11 @@ function dinoautogui_cdiv(){
 }
 
 var dino_over = Runner.prototype.gameOver
+var tcode = document.getElementById('t')
 
 dinoautogui_cdiv()
+
+dinolog('info', 'gui 창 코드 삽입');
 
 function dag_im_on(){
   let dagim = document.getElementById('dag_im');
@@ -31,6 +48,7 @@ function dag_im_on(){
   
   dagim.style.display = "block";
   dagmain.style.display = "none";
+  dinolog('info', 'dag_im_omn() 함수 호출');
 }
 
 function dag_sp_on(){
@@ -40,15 +58,35 @@ function dag_sp_on(){
   
   dagsp.style.display = "block";
   dagmain.style.display = "none";
+  dinolog('info', 'dag_sp_on() 함수 호출');
 }
 
-function dag_im_ex(){
+function dag_ex_on(){
   let dagex = document.getElementById('dag_ex');
   
   let dagmain = document.getElementById('dag_main');
   
   dagex.style.display = "block";
   dagmain.style.display = "none";
+    dinolog('info', 'dag_ex_on() 함수 호출');
+}
+
+function dag_exit(){
+  console.log('asdf')
+  dinolog('info', 'dinogui 삭제');
+  dinolog('thanks', '사용해주서셔 감사합니다. 다음에 또 이용해주세요.');
+  dinolog('info', '다시 불러오기 : 링크');
+  let dinoguidel = document.getElementById('dinogui');
+  let dinoguicssdel = document.getElementById('dinoguicss');
+  dinoguidel.remove();
+  dinoguicssdel.remove();
+  
+}
+
+function dagr(){
+  alert('새로고침 해주세요')
+  document.body.innerHTML = "[dinogui] 새로고침을 해주세요!"
+  document.head.innerHTML = "[dinogui] 새로고침을 해주세요!"
 }
 
 function back(){
@@ -60,6 +98,7 @@ function back(){
   dagmain1.style.display = "block";
   dagsp1.style.display = "none";
   dagex1.style.display = "none";
+  dinolog('info', 'back() 함수 호출');
 }
 
 dagim1 = document.getElementById('dag_im');
@@ -70,6 +109,17 @@ dagim1.style.display = "none";
 dagmain1.style.display = "block";
 dagsp1.style.display = "none";
 dagex1.style.display = "none";
+
+dinolog('info', 'gui 창 기초 설정 완료');
+
+function dag_setspeed(){
+ dagspeed = document.getElementById('dag_speed');
+ setspeed = dagspeed.value
+ setspeed *= 1
+ Runner.instance_.setSpeed(setspeed)
+ dinolog('info', 'speed 설정 값 : ' + dagspeed.value);
+}
+
 
 
 dragElement(document.getElementById("dinogui"));
@@ -107,3 +157,19 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+
+document.addEventListener("keydown", dinoguiposreset, false);
+
+dinolog('info', 'gui move 스크립트 로딩 완료');
+dinolog('info', 'OK!');
+
+function dinoguiposreset(e) {
+  if (e.keyCode === 81) {
+    document.getElementById('dinogui').style.left="600px";
+    document.getElementById('dinogui').style.top="600px";
+    alert("[dinogui] gui창 위치가 재설정 되었습니다");
+    dinolog('info', 'gui 창 위치 재설정');
+  }
+}
+
+dinolog('info', 'q 키를 누르시면 창 위치가 리셋됩니다!');
