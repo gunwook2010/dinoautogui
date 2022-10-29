1062ce8d1e352ae69afa47155d6e410654aa2813
gunwook2010 <57968248+gunwook2010@users.noreply.github.com>
2022-10-29 16:45:57 +0900

    Update dinoautogui.js

--- dinoautogui.js
+++ dinoautogui.js
@@ -19,7 +19,7 @@ function dinoautogui_cdiv(){
   gamecodet = document.createElement('div'); 
 
   code.setAttribute('id', 'dinogui'); 
-  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><div id='dag_main'><button onclick='dag_im_on();'>무적</button> <br> <button onclick='dag_sp_on()'>속도</button> <br> <button onclick='dag_ex_on()'>종료(wip)</button></div> <div id='dag_im'> <button onclick='Runner.prototype.gameOver = function (){};'>무적모드</button> <br> <button onclick='Runner.prototype.gameOver = dino_over;'>무적모드 끄기</button> <br> <button onclick='back()'>돌아가기</button></div> <div id='dag_sp'><input id='dag_speed' type='number' style='ime-mode:disabled;'></input><button onclick='dag_setspeed()'>적용</button><br><button onclick='back()'>돌아가기</button></div><div id='dag_ex'>개발중<br><button onclick='dag_exit()' style='background-color: red;'>종료</button><br><button onclick='dagr()' style='background-color: red;'>리셋</button><br><button onclick='back()'>돌아가기</button></div>"; 
+  code.innerHTML = "<div id='dinoguiheader'>dinoautogui</div><div id='dag_main'><button onclick='dag_im_on();'>무적</button> <br> <button onclick='dag_sp_on()'>속도</button> <br> <button onclick='dag_js_on()'>스크립트 실행</button> <br> <button onclick='dag_ex_on()'>종료(wip)</button></div> <div id='dag_im'> <button onclick='Runner.prototype.gameOver = function (){};'>무적모드</button> <br> <button onclick='Runner.prototype.gameOver = dino_over;'>무적모드 끄기</button> <br> <button onclick='back()'>돌아가기</button></div> <div id='dag_sp'><input id='dag_speed' type='number' style='ime-mode:disabled;'></input><button onclick='dag_setspeed()'>적용</button><br><button onclick='back()'>돌아가기</button></div> <div id='dag_js'><textarea id='dag_jsinput' placeholder='js code here'></textarea><button onclick='dag_runjs()'>적용</button><br><button onclick='back()'>돌아가기</button></div> <div id='dag_ex'>개발중<br><button onclick='dag_exit()' style='background-color: red;'>종료</button><br><button onclick='dagr()' style='background-color: red;'>리셋</button><br><button onclick='back()'>돌아가기</button></div>"; 
   
   game.appendChild(code); 
 
@@ -71,6 +71,16 @@ function dag_ex_on(){
     dinolog('info', 'dag_ex_on() 함수 호출');
 }
 
+function dag_js_on(){
+  let dagjs = document.getElementById('dag_js');
+  
+  let dagmain = document.getElementById('dag_main');
+  
+  dagjs.style.display = "block";
+  dagmain.style.display = "none";
+    dinolog('info', 'dag_js_on() 함수 호출');
+}
+
 function dag_exit(){
   console.log('asdf')
   dinolog('info', 'dinogui 삭제');
@@ -94,21 +104,25 @@ function back(){
   dagmain1 = document.getElementById('dag_main');
   dagsp1 = document.getElementById('dag_sp');
   dagex1 = document.getElementById('dag_ex');
+  dagjs1 = document.getElementById('dag_js');
   dagim1.style.display = "none";
   dagmain1.style.display = "block";
   dagsp1.style.display = "none";
   dagex1.style.display = "none";
   dinolog('info', 'back() 함수 호출');
+  dagjs1.style.display = "none";
 }
 
 dagim1 = document.getElementById('dag_im');
 dagmain1 = document.getElementById('dag_main');
 dagsp1 = document.getElementById('dag_sp');
 dagex1 = document.getElementById('dag_ex');
+dagjs1 = document.getElementById('dag_js');
 dagim1.style.display = "none";
 dagmain1.style.display = "block";
 dagsp1.style.display = "none";
 dagex1.style.display = "none";
+dagjs1.style.display = "none";
 
 dinolog('info', 'gui 창 기초 설정 완료');
 
@@ -120,7 +134,13 @@ function dag_setspeed(){
  dinolog('info', 'speed 설정 값 : ' + dagspeed.value);
 }
 
-
+function dag_runjs(){
+ dagspeed = document.getElementById('dag_jsinput');
+ runjs = dagspeed.value
+ runjst = runjs + ""
+ eval(runjst)
+ dinolog('info', 'js 실행 코드 : ' + runjs.value);
+}
 
 dragElement(document.getElementById("dinogui"));
 
